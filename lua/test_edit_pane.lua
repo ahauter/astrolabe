6e390f55de30319a9d9c6719a27604c77d7ae527
Austin <hautera@me.com>
2024-04-19 21:59:43 -0700

    Vertical split is nicer

--- lua/test_edit_pane.lua
+++ lua/test_edit_pane.lua
@@ -1,7 +1,5 @@
 local test_edit_buffer = nil
-local instruction_buffer = nil
 local window_id = nil
-local second_window_id = nil
 
 local M = {}
 
@@ -11,14 +9,6 @@ function M.SetBuffer(lines)
   end
   local num_lines = vim.api.nvim_buf_line_count(test_edit_buffer)
   vim.api.nvim_buf_set_lines(test_edit_buffer, 0, num_lines + 1, false, lines)
-end
-
-function M.AddInstructions(lines)
-  if instruction_buffer == nil then
-    return
-  end
-  local num_lines = vim.api.nvim_buf_line_count(instruction_buffer)
-  vim.api.nvim_buf_set_lines(instruction_buffer, 0, num_lines + 1, false, lines)
 end
 
 --TODO Maybe add a vertical split in buffer
@@ -35,30 +25,10 @@ function M.MakePopup()
   local width = 100
   local height = 40
   local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-  local second_wind_id, win = popup.create(instruction_buffer, {
-    title = "Astrolabe",
-    highlight = "Astrolabe",
-    focusable = false,
-    line = math.floor(((vim.o.lines - height) / 2) - 1),
-    col = math.floor((vim.o.columns - width) / 2),
-    minwidth = width,
-    minheight = 5,
-    borderchars = borderchars,
-  })
-  local win_id, win = popup.create(test_edit_buffer, {
-    line = math.floor(((vim.o.lines - height) / 2) + 5),
-    col = math.floor((vim.o.columns - width) / 2),
-    minwidth = width,
-    minheight = height - 5,
-    borderchars = borderchars,
-  })
+  vim.cmd("vsplit")
+  local win_id = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_buf(win_id, test_edit_buffer)
   window_id = win_id
-  second_window_id = second_wind_id
-  M.AddInstructions({
-    "q to Quit",
-    "a to Append to to <test_file>",
-    "c to Create new test file"
-  })
   M.SetBuffer({
     "q to Quit",
     "a to Append to to <test_file>",
@@ -77,7 +47,6 @@ end
 
 function CloseTestWindow()
   vim.api.nvim_win_close(window_id, true)
-  vim.api.nvim_win_close(second_window_id, true)
   window_id = nil
   test_edit_buffer = nil
 end
