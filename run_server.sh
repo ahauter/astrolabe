218ad6db28a71ff71eebbb5007854399b8f24109
Austin Hauter <hautera@me.com>
2025-11-29 07:32:13 -0800

    New run server scripts

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,3 @@
-llama-server -hf TheBloke/deepseek-coder-6.7B-instruct-GGUF -ngl 99 -t 28 -dev CUDA0 -c 2048 -n 120
+llama-server \
+  -hf Qwen/Qwen2.5-Coder-0.5B-Instruct-GGUF:Q8_0 \
+  -ngl 99 -t 28 -dev CUDA0 -c 2048 -n 64
---
+++ run_server_md.sh
@@ -0,0 +1,1 @@
+llama-server -hf Qwen/Qwen2.5-Coder-14B-Instruct-GGUF:Q5_K_M -ngl 99 -t 28 -dev CUDA0 -c 2048 -n 256
